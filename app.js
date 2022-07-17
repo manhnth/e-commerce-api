7bbf92d78a9082ddba8771ec6f1bb2b4a0b3b877
manhtheN <themanh2k@gmail.com>
2022-07-17 16:04:22 +0700

    build: setup basic express server

---
+++ app.js
@@ -0,0 +1,37 @@
+require('dotenv').config();
+require('express-async-errors');
+
+
+const express = require('express');
+const app = express();
+
+// database 
+const connectDB = require('./db/connect');
+
+// routers
+const authRouter = require('./routes/authRoutes');
+const userRouter = require('./routes/userRoutes');
+const productRouter = require('./routes/productRoutes');
+const orderRouter = require('./routes/orderRoutes');
+const reviewRouter = require('./routes/reviewRoutes');
+
+app.get('/', (req, res) => {
+  res.send('server')
+})
+app.use('/api/v1/auth', authRouter);
+// app.use('/api/v1/users', userRouter);
+// app.use('/api/v1/products', productRouter);
+// app.use('/api/v1/orders', orderRouter);
+// app.use('api/v1/reviews', reviewRouter);
+
+const port = process.env.PORT || 5000;
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URL)
+    app.listen(port, () => console.log(`Server is listening on port ${port}`))
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
